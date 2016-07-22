5d0e76a015dffef98d50bcc1577e7efa78048912
Thiago Adriano <thiago.adriano@kiper.com.br>
2016-07-22 15:15:47 -0300

    correcao reboot

--- reboot.sh
+++ reboot.sh
@@ -2,11 +2,20 @@
 echo "insira o ramal desejado para o reboot"
 read ramal
 echo "verificando ip do ramal $ramal"
-ip=`asterisk -rx "sip show peer $ramal" |grep Addr |cut -c 18-40 |sed "s/ Port 5060//g"`
+ip=`asterisk -rx "sip show peer $ramal" |grep Reg. |cut -c 22-50 |sed "s/$ramal@//g" | sed "s/:5060//g"`
 echo "o ip é $ip"
 sleep 2
 echo "rebootando ramal $ramal"
 curl -d pass=k1p3r POST http://$ip:8085
 sleep 3
 curl http://$ip:8085/A0
+echo "rebootando ramal $ramal"
+curl -d pass=k1p3r POST http://$ip:8085
+sleep 3
+curl http://$ip:8085/A0
+echo "rebootando ramal $ramal"
+curl -d pass=k1p3r POST http://$ip:8085
+sleep 3
+curl http://$ip:8085/A0
+
 echo "E todos viveram felizes para sempre..."
